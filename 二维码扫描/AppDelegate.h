f2a2bc833ef7bb122c76cb6130725fa8f8190893
huhang <huhangatzz@163.com>
2015-11-27 16:10:18 +0800

    Initial Commit

---
+++ 二维码扫描/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  二维码扫描
+//
+//  Created by huhang on 15/11/27.
+//  Copyright (c) 2015年 huhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 二维码扫描/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  二维码扫描
+//
+//  Created by huhang on 15/11/27.
+//  Copyright (c) 2015年 huhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
